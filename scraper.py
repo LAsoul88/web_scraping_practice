e5ee01656553e4137bec0b6bd8de52bd3954c486
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-09-23 15:08:49 -0700

    switched to chrome and successfully navigating into vid

--- scraper.py
+++ scraper.py
@@ -1,22 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 urls = ["https://www.tiktok.com/explore"]
 
-options = webdriver.FirefoxOptions()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
+# options = webdriver.FirefoxOptions()
+# options.headless = True
+# options.add_argument("--window-size=1920,1200")
 
-print('thusly')
-driver = webdriver.Firefox(options=options)
-print('here')
+# driver = webdriver.Firefox(options=options)
+driver = webdriver.Chrome()
 driver.get(urls[0])
-print(driver)
-singing_and_dancing = driver.find_element(By.XPATH, '/html/body/div/div')
-print(singing_and_dancing)
-singing_and_dancing.find_element(By.XPATH, 'div')
-driver.quit()
+
+try:
+  driver.implicitly_wait(2)
+  singing_and_dancing = driver.find_element(By.XPATH, '//*[@id="main-content-explore_page"]/div/div[1]/div/button[2]')
+  singing_and_dancing.click()
+  
+  videos = driver.find_element(By.XPATH, '//*[@id="main-content-explore_page"]/div/div[2]/div/div[1]/div[1]/div/div/a/div/div[1]/div/span/picture')
+
+  first_vid = videos.find_element(By.XPATH, '//img')
+  driver.implicitly_wait(2)
+  print(first_vid)
+  driver.execute_script("arguments[0].click();", first_vid)
+  driver.implicitly_wait(4)
+  song = driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[4]/div/div[2]/div[1]/div/div[1]/div[1]/div[2]/h4/a/div')
+  print(song.text)
+
+  
+except Exception as e:
+  print(e)
+
+# driver.quit()
 # while len(urls) > 0:
 #   current_url = urls.pop()
 
@@ -39,14 +55,3 @@ driver.quit()
 # download = driver.find_element(By.XPATH, '/html/body/forge-dialog/div/forge-scaffold/div[3]/forge-toolbar/forge-button[2]/button').click()
 # print(download)
 # driver.quit()
-
-# page = requests.get(url)
-
-# soup = BeautifulSoup(page.text, features='html.parser')
-
-# print(soup)
-
-# refined_soup = soup.find(class_='action-button-wrapper')
-# print(refined_soup.prettify())
-# next_soup = refined_soup.find(class_='info-pane')
-# print(next_soup)
