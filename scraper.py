71f3ff7c7d276e64659cc67f314c6a1b4e0d5df3
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-09-22 20:31:12 -0700

    attempting to find specific button in selenium

--- scraper.py
+++ scraper.py
@@ -2,17 +2,41 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+urls = ["https://www.tiktok.com/explore"]
 
 options = webdriver.FirefoxOptions()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 
+print('thusly')
 driver = webdriver.Firefox(options=options)
-url = 'https://data.texas.gov/dataset/Mixed-Beverage-Gross-Receipts/naix-2893/about_data'
-driver.get(url)
+print('here')
+driver.get(urls[0])
+print(driver)
+singing_and_dancing = driver.find_element(By.XPATH, '/html/body/div/div')
+print(singing_and_dancing)
+singing_and_dancing.find_element(By.XPATH, 'div')
+driver.quit()
+# while len(urls) > 0:
+#   current_url = urls.pop()
 
-button = driver.find_element(By.XPATH, '/html/body/main/div/div[1]/div/div[2]/div/div/div/forge-button/button').click()
-download = driver.find_element(By.XPATH, '/html/body/forge-dialog/div/forge-scaffold/div[3]/forge-toolbar/forge-button[2]/button').click()
+#   response = requests.get(current_url)
+#   soup = BeautifulSoup(response.content, "html.parser")
+#   print(soup)
+
+#   button_elements = soup.select("button")
+#   print(button_elements)
+#   for button in button_elements:
+#     url = button['href']
+#     if "https://www.tiktok.com/" in url:
+#       urls.append(url)
+
+# print(urls)
+
+
+
+# button = driver.find_element(By.XPATH, '/html/body/main/div/div[1]/div/div[2]/div/div/div/forge-button/button').click()
+# download = driver.find_element(By.XPATH, '/html/body/forge-dialog/div/forge-scaffold/div[3]/forge-toolbar/forge-button[2]/button').click()
 # print(download)
 # driver.quit()
 
